6c7d0e68a2919935978d51543bdadfa342317d98
Daniel <danielbauman1010@gmail.com>
2015-08-10 22:05:12 -0400

    initial commit

---
+++ FileEditor.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fileeditor;
+
+import java.awt.Frame;
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author daniel
+ */
+public class FileEditor {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        GUI gui = new GUI();
+        gui.setVisible(true);
+    }
+    
+}
